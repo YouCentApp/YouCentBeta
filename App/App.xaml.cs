d9fad3c15d5ef69be3fedf3f78778b73e4690416
Ark Fen <arkfen@gmail.com>
2023-04-02 17:37:41 +0800

    Initial commit - empty MAUI project

---
+++ App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
