db29bef3182a66bc21f101e9acd9f9f9f8bf3828
Ark Fen <arkfen@gmail.com>
2023-08-15 15:13:09 +0800

    Design experiments and some cleaning

--- BlazorSite/Program.cs
+++ BlazorSite/Program.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorSite;
-using PromiseLib.Weather;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient {
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSingleton<WeatherForecastService>();
+
 
 await builder.Build().RunAsync();
 
